8f8e851692ee63649d82c054df5a405e6a672f2a
kobia <josephacquah@Josephs-MacBook-Pro.local>
2026-04-24 16:46:19 -0230

    refactor: update Seneca act calls with explicit error handling
    Replace simple console.log with error-checked callback for sum command

--- index.js
+++ index.js
@@ -9,6 +9,27 @@
 // seneca code
 var seneca = require('seneca')();
 
+// //adding a service to seneca
+// seneca.add({role: 'math', cmd:'sum'}, function (msg, respond){
+//     var sum = msg.left + msg.right;
+//     respond(null, {answer: sum})
+// })
+
+// //another service
+// seneca.add({role: 'math', cmd:'product'}, function(msg, respond){
+//     var product = msg.left * msg.right;
+//     respond(null, {answer: product})
+// })
+
+// // call the seneca service(also referred to as send something to the seneca
+// // service and whatever has been programmed inside will handle your request)
+
+// seneca.act({role: 'math', cmd: 'sum', left: 2, right: 3}, console.log);
+// // console.log will be converted by respond(null, {answer: product}) and it'll appear as `console.log(null, {answer: product})`
+
+// seneca.act({role: 'math', cmd: 'product', left: 2, right: 3}, console.log);
+
+// start
 //adding a service to seneca
 seneca.add({role: 'math', cmd:'sum'}, function (msg, respond){
     var sum = msg.left + msg.right;
@@ -21,11 +42,12 @@ seneca.add({role: 'math', cmd:'product'}, function(msg, respond){
     respond(null, {answer: product})
 })
 
-// call the seneca service(also referred to as send something to the seneca
-// service and whatever has been programmed inside will handle your request)
 
-seneca.act({role: 'math', cmd: 'sum', left: 2, right: 3}, console.log);
-// console.log will be converted by respond(null, {answer: product}) and it'll appear as `console.log(null, {answer: product})`
+seneca.act({role: 'math', cmd:'sum', left: 2, right: 3}, function(err, data){
+    if(err){
+        return console.error(err)
+    }
+    console.log(data)
+})
 
-seneca.act({role: 'math', cmd: 'product', left: 2, right: 3}, console.log);
-
+seneca.act({role: 'math', cmd: 'product', left: 2, right: 3}, console.log)
