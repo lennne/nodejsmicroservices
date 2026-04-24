7fa7cfcc7454d2c0e3d8d6f1db84288ce967036d
kobia <josephacquah@Josephs-MacBook-Pro.local>
2026-04-24 14:33:42 -0230

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-type': 'text/plain'});
+    res.end('hello world');
+})
+
+server.listen(8000);
