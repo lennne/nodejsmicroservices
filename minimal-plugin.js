00ac7f04c5a9717179a91ff67459b33996b15291
kobia <josephacquah@Josephs-MacBook-Pro.local>
2026-04-24 18:07:43 -0230

    feat: transform minimal-plugin into functional math module
    Implement init action to manage plugin lifecycle and setup
    
    Demonstrate API aggregation by combining service definition with initialization

--- minimal-plugin.js
+++ minimal-plugin.js
@@ -1,8 +1,23 @@
-function minimal_plugin(options){
-    console.log(options);
+function init(msg, respond){
+    console.log('plugin initialized');
+    console.log('expensive operation taking place now....DONE')
+    respond();
+}
+
+
+function math(options){
+    this.add({role: 'math', cmd: 'sum'}, function (msg, respond){
+        respond(null, {answer: msg.left + msg.right})
+    })
+
+    this.add({role: 'math', cmd: 'product'}, function(msg, respond){
+        respond(null, {answer: msg.left * msg.right})
+    })
+
+    this.add({init: 'math'}, init);
 }
 
 //immediately executes the 'minimal_plugin' function
 //an example of the plugin pattern, API aggregation, best practice for building applications
 //and what is this concept? small pieces of software being combined to create a bigger system
-require('seneca')().use(minimal_plugin, {foo: 'bar'})
+require('seneca')().use(math).act('role: math, cmd:sum, left:1, right:2', console.log);
