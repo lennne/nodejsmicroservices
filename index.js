e29deb256d88411a0fe80c6a9c1dcc23a2bf9e08
kobia <josephacquah@Josephs-MacBook-Pro.local>
2026-04-24 15:06:43 -0230

    feat: implement simple logic in microservices using Seneca
    Add sum and product command patterns for math role
    
    Demonstrate service execution with seneca.act calls

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-type': 'text/plain'});
-    res.end('hello world');
+// var http = require('http');
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, { 'Content-type': 'text/plain'});
+//     res.end('hello world');
+// })
+
+// server.listen(8000);
+
+// seneca code
+var seneca = require('seneca')();
+
+//adding a service to seneca
+seneca.add({role: 'math', cmd:'sum'}, function (msg, respond){
+    var sum = msg.left + msg.right;
+    respond(null, {answer: sum})
 })
 
-server.listen(8000);
+//another service
+seneca.add({role: 'math', cmd:'product'}, function(msg, respond){
+    var product = msg.left * msg.right;
+    respond(null, {answer: product})
+})
+
+// call the seneca service(also referred to as send something to the seneca
+// service and whatever has been programmed inside will handle your request)
+
+seneca.act({role: 'math', cmd: 'sum', left: 2, right: 3}, console.log);
+// console.log will be converted by respond(null, {answer: product}) and it'll appear as `console.log(null, {answer: product})`
+
+seneca.act({role: 'math', cmd: 'product', left: 2, right: 3}, console.log);
+
